d331ef7ef2f5154a7570356603d6a9bed0705961
Tomek <klips0303@gmail.com>
2019-12-07 14:01:05 +0100

    Merge branch 'daltestas_feature' of C:\Users\User\Desktop\gitworkshops with conflicts.

--- src/Main2.java
+++ src/Main2.java
@@ -6,7 +6,7 @@
  **/
 public final class Main2 {
     public static void main(String[] args) {
-        System.out.println("New class");
+        System.out.println("New class22qqssss22222");
     }
 
 }
