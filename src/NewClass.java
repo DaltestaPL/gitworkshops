4610e73499e9f6b1c5cb080b98378fc01431028b
Tomek <klips0303@gmail.com>
2019-12-07 11:39:11 +0100

    Pierwszy conmmit do projektu

---
+++ src/NewClass.java
@@ -0,0 +1,11 @@
+/**
+ * ... comment class...
+ *
+ * @author tomasz.wojtyra.tw@gmail.com
+ * @since 07.12.2019
+ **/
+final class NewClass {
+    public static void main(String[] args) {
+
+    }
+}
