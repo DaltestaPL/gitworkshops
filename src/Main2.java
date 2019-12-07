6207cafaa73dddfd0267d088aadaf9a63e57c741
Denys <d.skeylz@gmail.com>
2019-12-07 13:33:05 +0100

    Denys

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+/**
+ * ... comment class...
+ *
+ * @author d.skeylz@gmail.com
+ * @since 07.12.2019
+ **/
+public final class Main2 {
+    public static void main(String[] args) {
+        System.out.println("New class");
+    }
+
+}
