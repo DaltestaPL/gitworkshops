0c0c4819f448df660f862aed03d8dfdd821c75aa
Pawel <hyzy1988@o2.pl>
2019-12-07 14:49:56 +0100

    zad.5 prostokąt

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,21 @@
  * @author tomasz.wojtyra.tw@gmail.com
  * @since 07.12.2019
  **/
+
+
+
 final class Main {
     public static void main(String[] args) {
-        
+        Rectangle rect = new Rectangle(10, 5);
+        float P = rect.fieldRectangle();
+        System.out.println("Pole prostokąta: " + P);
+
+        float O = rect.circumferenceRectangle();
+        System.out.println("Obwód prostokąta: " + O);
+
+        double przekatna = rect.diagonal();
+        System.out.println("Przekątna prostokąta: " + przekatna);
+
+
     }
 }
