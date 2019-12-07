7094a4dc78229d24220b5a44f94dd6b6c3ecf4c4
Mateusz Pogonowski <pogonowskimateusz@gmail.com>
2019-12-07 13:16:44 +0100

    Name change

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@
  **/
 final class Main {
     public static void main(String[] args) {
-        System.out.println("Program");
+        System.out.println("Mateusz");
         System.out.println("Hello, hello");
         System.out.println("Uwaga nowa zmiana!");
     }
